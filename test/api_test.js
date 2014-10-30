aa6e49b868d3918750745c3c6688fe8968e545b0
Charles Renwick <crenwick@Charless-MacBook-Pro-2.local>
2014-10-30 13:49:42 -0700

    Fixed async and module exports

--- test/api_test.js
+++ test/api_test.js
@@ -1,4 +1,6 @@
+process.env.NODE_ENV = 'TEST';
 /*jshint node: true*/
+'use strict';
 var chai = require('chai');
 var chaihttp = require('chai-http');
 var app = require('../server');
@@ -6,23 +8,26 @@ chai.use(chaihttp);
 
 describe('Simple JSON API', function () {
     //var data;
-    it("/time/ should get the time", function () {
-        chai.request('http://localhost:3000')
+    it("/time/ should get the time", function (done) {
+        chai.request(app)
             .get('/time')
             .end(function (err, res) {
                 chai.expect(err).to.be.null;
                 console.log('minute is: ' + res.body.minute);
                 chai.expect(res.body.minute).to.equal(new Date().getMinutes());
+                done();
             });
+
     });
 
-    it('/name/:name should greet someone', function () {
+    it('/name/:name should greet someone', function (done) {
         chai.request('http://localhost:3000')
             .get('/name/Charles')
             .end(function (err, res) {
                 chai.expect(err).to.be.null;
                 console.log(res.body);
                 chai.expect(res.body.msg).to.equal('Hello, Charles!');
+                done();
             });
     });
 });
